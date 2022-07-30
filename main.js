1a9292a7b8f71337da386927881fb418a51b9e23
jaysharma21 <jaysharma2198@gmail.com>
2022-07-30 19:11:33 +0530

    setting us JS

---
+++ main.js
@@ -0,0 +1,26 @@
+let shop=document.getElementById("shop");
+
+let shopItemsData = [{
+    id:"",
+    name: "Casual Shirt",
+    price: "$45",
+    desc: "lorem7"
+},{},{},{}]
+
+let generateShop = ()=>{
+    return (shop.innerHTML=`<div class="item">
+    <img width="220" src="/images/img-1.jpg" alt="">
+    <div class="details">
+        <h3>Casual Shirt</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        <div class="price-quantity">
+            <h2>$ 45</h2>
+            <div class="buttons">
+                <i class="bi bi-dash-lg"></i>
+                <div class="quantity">0</div>
+                <i class="bi bi-plus-lg"></i>
+            </div>
+        </div>
+    </div>
+</div>`)
+}
